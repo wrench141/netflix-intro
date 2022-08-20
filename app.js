18d0bbfeb9574d1850fed6a2b550d07b803df30d
programmer141-dev <sidhardhchandra141@gmail.com>
2022-08-20 13:17:03 +0530

    animation remastered

--- app.js
+++ app.js
@@ -1,39 +1,66 @@
-const stick3 = document.getElementsByClassName('stick3')[0]
-const stick2 = document.getElementsByClassName('color')[0]
-const stick1 = document.getElementsByClassName('stick1')[0]
+const brush = document.getElementsByClassName('brush')[0]
 const lines = document.getElementsByClassName('lines')[0]
-const color1 = document.getElementsByClassName('color1')[0]
 
-const generateLines = () => {
-    for (let i = 0; i <= 50; i++) {
-        var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
-        var randomWidth = Math.floor(Math.random() * 7 + i)
-        var randomNum = Math.floor(Math.random() * 7 )
-
-        var elem = document.createElement('div');
-        elem.style.position = 'relative'
-        elem.style.zIndex = 0
-        elem.style.backgroundColor = randomColor === '#000000' ? 'white' : randomColor;
-        elem.style.width = randomNum + 'px';
-        elem.className = "line";
-        lines.appendChild(elem);
-    }
-
-}
+const stick1 = document.getElementsByClassName('color2')[0]
+const stick2 = document.getElementsByClassName('brush3')[0]
+const stick3 = document.getElementsByClassName('color')[0]
 
 setTimeout(() => {
-    stick3.style.animation = 'stick-reverse 1s ease-in forwards'
-}, 4000);
+    stick1.style.animation = 'movedown 0.5s ease forwards'
+}, 3000)
 
 setTimeout(() => {
-    stick2.style.animation = 'stick-reverse 1s ease-in forwards'
-}, 5000);
+    stick2.style.animation = 'movedown 0.5s ease forwards'
+}, 3500)
 
 setTimeout(() => {
-    stick1.style.animation = 'stick-change 1s ease forwards'
-    lines.style.animation = 'c-change 1s ease forwards'
-    color1.style.animation = 'opacity 1s ease forwards'
-    generateLines()
-}, 6000)
+    stick3.style.animation = 'opacity 0.5s linear infinite'
+    lines.style.display = 'flex'
+}, 4000)
+
+const colorArray = [
+'#133286',
+'#3062af',
+'#FFF',
+'#4a7fcb',
+    '#133286',
+    '#3062af',
+'#628ace',
+'#949fd9',
+    '#821e12',
+    '#c34821',
+    'red',
+'#d3ad94',
+'yellow',
+'#821e12',
+    '#462652',
+    '#b16f67',
+    '#d3ad94',
+    '#821e12',
+    '#462652',
+    '#4a7fcb',
+    '#133286',
+    '#3062af',
+    '#628ace',
+    '#b16f67',
+    '#d3ad94',
+    'yellow',
+    '#821e12',
+    '#821e12',
+    '#c34821',
+'#821e12',
+'#c34821',
+'red',
+]
+
+
+colorArray.map((color) => {
+    const elem = document.createElement('div');
+    const randomWidth = Math.floor(Math.random() * 1000);
+    elem.className = 'line';
+    elem.style.setProperty('--m', `${randomWidth}px`)
+    elem.style.setProperty('--c', `${color}`)
+    lines.appendChild(elem)
+})
 
 
