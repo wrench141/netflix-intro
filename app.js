4ff29b959f63b5631c6b93b87f04e76f96dcd481
programmer141-dev <sidhardhchandra141@gmail.com>
2022-08-21 11:22:48 +0530

    netflix-animation

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
-const brush = document.getElementsByClassName('brush')[0]
+const stick1 = document.getElementsByClassName('color')[0]
+const stick2 = document.getElementsByClassName('color2')[0]
+const stick3 = document.getElementsByClassName('brush3')[0]
 const lines = document.getElementsByClassName('lines')[0]
 
-const stick1 = document.getElementsByClassName('color2')[0]
-const stick2 = document.getElementsByClassName('brush3')[0]
-const stick3 = document.getElementsByClassName('color')[0]
-
+// make reverse animations trigger
 setTimeout(() => {
-    stick1.style.animation = 'movedown 0.5s ease forwards'
+    stick2.style.animation = 'movedown 0.5s ease forwards'
 }, 3000)
 
 setTimeout(() => {
-    stick2.style.animation = 'movedown 0.5s ease forwards'
+    stick3.style.animation = 'movedown 0.5s ease forwards'
 }, 3500)
 
 setTimeout(() => {
-    stick3.style.animation = 'opacity 0.5s linear infinite'
+    stick1.style.animation = 'opacity 0.5s ease forwards'
     lines.style.display = 'flex'
 }, 4000)
 
-const colorArray = [
-'#133286',
-'#3062af',
-'#FFF',
-'#4a7fcb',
+
+// lets create some random lines width random margins or gaps
+
+let colors = [
     '#133286',
     '#3062af',
-'#628ace',
-'#949fd9',
+    '#FFF',
+    '#4a7fcb',
+    '#133286',
+    '#3062af',
+    '#628ace',
+    '#949fd9',
     '#821e12',
     '#c34821',
     'red',
-'#d3ad94',
-'yellow',
-'#821e12',
+    '#d3ad94',
+    'yellow',
+    '#821e12',
     '#462652',
     '#b16f67',
     '#d3ad94',
@@ -48,19 +50,17 @@ const colorArray = [
     '#821e12',
     '#821e12',
     '#c34821',
-'#821e12',
-'#c34821',
-'red',
+    '#821e12',
+    '#c34821',
+    'red',
 ]
 
+colors.map((color) => {
+    const line = document.createElement('div');
+    const randomMargin = Math.floor(Math.random() * 1000);
 
-colorArray.map((color) => {
-    const elem = document.createElement('div');
-    const randomWidth = Math.floor(Math.random() * 1000);
-    elem.className = 'line';
-    elem.style.setProperty('--m', `${randomWidth}px`)
-    elem.style.setProperty('--c', `${color}`)
-    lines.appendChild(elem)
+    line.className = 'line';
+    line.style.setProperty('--m', `${randomMargin}px`);
+    line.style.setProperty('--c', color);
+    lines.appendChild(line)
 })
-
-
